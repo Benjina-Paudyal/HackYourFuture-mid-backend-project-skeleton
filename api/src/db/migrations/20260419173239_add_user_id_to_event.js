0ef71151cb37074bf5ee8c040587b4c940542c12
benjina-paudyal <benjina.paudyal@gmail.com>
2026-04-19 20:21:04 +0200

    feat: week1: clean database setup

---
+++ api/src/db/migrations/20260419173239_add_user_id_to_event.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.alterTable("event", (table) => {
+    table.integer("user_id")
+      .unsigned()
+      .references("id")
+      .inTable("app_user")
+      .onDelete("SET NULL");
+  });
+}
+
+export async function down(knex) {
+  await knex.schema.alterTable("event", (table) => {
+    table.dropColumn("user_id");
+  });
+}
