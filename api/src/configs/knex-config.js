49138fb91b56b364981361e6b5b99cf2a89fe989
benjina-paudyal <benjina.paudyal@gmail.com>
2026-05-11 19:33:14 +0200

    Added env for Neon

--- api/src/configs/knex-config.js
+++ api/src/configs/knex-config.js
@@ -1,37 +1,44 @@
 export function createKnexConfig() {
-    const client = process.env.DB_CLIENT ?? "pg";
+  const client = process.env.DB_CLIENT ?? "pg";
 
-    const isSqlite = client === "sqlite3";
+  const isSqlite = client === "sqlite3";
 
-    return {
-        client,
+  return {
+    client,
 
-        connection: isSqlite
-            ? {
-                filename: process.env.DB_SQLITE_FILENAME ?? "src/db/database.sqlite",
-              }
-            : {
-                host: process.env.DB_HOST,
-                port: process.env.DB_PORT,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_DATABASE_NAME,
-                ssl:
-                    process.env.DB_USE_SSL === "true"
-                        ? { rejectUnauthorized: false }
-                        : false,
-                
-                searchPath: ['public'],
-              },
+    connection: isSqlite
+      ? {
+          filename: process.env.DB_SQLITE_FILENAME ?? "src/db/database.sqlite",
+        }
+      : process.env.DATABASE_URL
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          }
+        : {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE_NAME,
+            ssl:
+              process.env.DB_USE_SSL === "true"
+                ? { rejectUnauthorized: false }
+                : false,
 
-        useNullAsDefault: isSqlite,
+            searchPath: ["public"],
+          },
 
-        migrations: {
-            directory: "src/db/migrations",
-        },
+    useNullAsDefault: isSqlite,
 
-        seeds: {
-            directory: "src/db/seeds",
-        },
-    };
+    migrations: {
+      directory: "src/db/migrations",
+    },
+
+    seeds: {
+      directory: "src/db/seeds",
+    },
+  };
 }
